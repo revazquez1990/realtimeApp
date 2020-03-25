eec1bdc0c9749755493dad4cded3972a5c126a84
unknown <revazquez1990@gmail.com>
2020-03-25 15:38:57 -0400

    CRUD for Category

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 
-Route::get('/question', 'QuestionController@index');
+/* Route::get('/question', 'QuestionController@index');
 Route::get('/question/{question}', 'QuestionController@show');
-Route::delete('/question/{question}', 'QuestionController@destroy');
+Route::delete('/question/{question}', 'QuestionController@destroy'); */
+Route::apiResource('/question', 'QuestionController');
 
+Route::apiResource('/category', 'CategoryController');
 
