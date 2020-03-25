e900d2b2ab544dd567a7efb349a729932ba7c8e4
unknown <revazquez1990@gmail.com>
2020-03-25 17:19:00 -0400

    Like and Dislike system for reply

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,17 @@
 /* Route::get('/question', 'QuestionController@index');
 Route::get('/question/{question}', 'QuestionController@show');
 Route::delete('/question/{question}', 'QuestionController@destroy'); */
+
+use Illuminate\Routing\Route;
+
 Route::apiResource('/question', 'QuestionController');
 
 Route::apiResource('/category', 'CategoryController');
 
 Route::apiResource('/question/{question}/reply', 'ReplyController');
+
+Route::post('/like/{reply}', 'LikeController@likeIt');
+
+Route::delete('/like/{reply}', 'LikeController@unkeIt');
+
 
